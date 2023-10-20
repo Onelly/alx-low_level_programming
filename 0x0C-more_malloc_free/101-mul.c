772343f6790acbdec692c6f6824e25ed4adab6a7
Onelly <eneliolisa@gmail.com>
2023-10-20 12:10:55 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -56,8 +56,8 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 	for (addrem += mulrem; k >= 0 && addrem; k--)
 	{
 		add = (dest[k] - '0') + addrem;
-		addrem = add / 10; -
-			dest[k] = add % 10 + '0';
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
 	}
 
 	if (addrem)
